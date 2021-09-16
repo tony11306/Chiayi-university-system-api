5cbddd1d29b37051f933c4cd218b57361bda5d6a
Tony11306 <tony20020507@gmail.com>
2021-09-16 16:34:42 +0800

    move add_resouce() to outside the main

--- main.py
+++ main.py
@@ -8,16 +8,15 @@ from SchoolSystemModel.course import CourseEndpoint
 from SchoolSystemModel.login import LoginEndpoint
 from SchoolSystemModel.grades import GradeEndpoint
 
+app.config['JSON_AS_ASCII'] = False
+app.config['JSONIFY_MINETYPE'] = 'application/json;charset=utf-8'
+app.config.update(RESTFUL_JSON=dict(ensure_ascii=False))
+
+api.add_resource(CourseEndpoint, '/course/<string:webpid1>')
+api.add_resource(LoginEndpoint, '/login/<string:account>/<string:password>')
+api.add_resource(GradeEndpoint, '/grade/<string:webpid1>')
+
 if __name__ == '__main__':
-    app.config['JSON_AS_ASCII'] = False
-    app.config['JSONIFY_MINETYPE'] = 'application/json;charset=utf-8'
-    app.config.update(RESTFUL_JSON=dict(ensure_ascii=False))
-
-
-    api.add_resource(CourseEndpoint, '/course/<string:webpid1>')
-    api.add_resource(LoginEndpoint, '/login/<string:account>/<string:password>')
-    api.add_resource(GradeEndpoint, '/grade/<string:webpid1>')
-
     app.run(debug=False)
 
 
