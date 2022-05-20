ed395d676be118d26040daf680733bd0d0452eea
Tony11306 <tony20020507@gmail.com>
2022-05-20 18:09:56 +0800

    update course_selection api add "ecourse 線上" option in campus add "通識教育選修選項：通識網路課程" in course_classification

--- SchoolSystemModel/course_selection.py
+++ SchoolSystemModel/course_selection.py
@@ -58,9 +58,6 @@ class CourseSelectionEndpoint(Resource):
 
         result = []
 
-        if campus is not None and campus == 'ecourse 線上':
-            campus = 'https：//ecourse. ncyu. edu. tw'
-
         for course in _data:
             if campus is not None and course['校區'] != campus:
                 continue
